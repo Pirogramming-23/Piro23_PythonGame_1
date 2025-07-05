df769fb1c67f7f04578fb2b55bde8473a6cf6c40
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-06 07:49:20 +0900

    main.py에 추가

--- jieun.py
+++ jieun.py
@@ -2,16 +2,24 @@
 import random
 import time
 
+######## 아파트 게임 함수 ########
 # 총 player를 list로 받음 - 추후 수정 가능
-def gameJieun(player_list):
+def gameJieun(player_list, host, user_name):
+    
     print("아 파트 아파트~🎶 아 파트 아파트~~🎶🎶")
+    
     # input 받기
-    while True:
-        floor = int(input("층 수를 입력해주세요(5 이상의 정수) : "))
-        if floor < 5:
-            print("5 이상의 숫자를 입력해주세요!")
-        else:
-            break
+    if host == user_name:
+        while True:
+            floor = int(input("층 수를 입력해주세요(5 이상 30 이하의 정수) : "))
+            if floor < 5:
+                print("5 이상의 숫자를 입력해주세요")
+            elif floor > 30:
+                print("30 이하의 숫자를 입력해주세요")
+            else:
+                break
+    else:
+        floor = random.randint(5, 30)
         
     print(f"{floor}층!")
     time.sleep(1)
@@ -31,12 +39,14 @@ def gameJieun(player_list):
             if call_num >= floor :
                 print(f"{hand} : {call_num} ..")
                 target = hand.split(" ")[0]
-                return target
                 break
             print(f"{hand} : {call_num}!")
     
-    
+    print(f"아 누가 술을 마셔 {target}(이)가 술을 마셔~~ 원~샷!")
+    return target
     
 if __name__ == "__main__":
     player_list = [1, 2, 3, 4]
-    gameJieun(player_list)
+    #gameJieun(player_list, 1, 3) # 컴퓨터 진행 게임
+    gameJieun(player_list, 1, 1) # 사용자 진행 게임
+    
